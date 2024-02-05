d89a82abf05214f9ff9324d82162395d6adb949a
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-05 07:09:38 +0100

    Update build.config.ts

--- build.config.ts
+++ build.config.ts
@@ -1,6 +1,6 @@
 /// <reference lib="deno.ns" />
 import * as esbuild from 'https://deno.land/x/esbuild@v0.19.12/mod.js';
-import esbuildPluginSass from 'https://deno.land/x/esbuild_plugin_sass@v0.4.2/mod.ts';
+import esbuildPluginSass from 'https://deno.land/x/esbuild_plugin_sass@v0.5.0/mod.ts';
 import { green } from 'https://deno.land/std@0.211.0/fmt/colors.ts';
 import { parseArgs } from 'https://deno.land/std@0.211.0/cli/parse_args.ts';
 
